1b463a5e6cc39ad32acd6159f7ffa42122bec430
github username <charliepbarth@gmail.com>
2021-02-02 20:19:50 -0500

    Added spaceship class

---
+++ frontend/scripts/models/spaceship.js
@@ -0,0 +1,52 @@
+class Spaceship {
+
+//   constructor(){
+//       this._display 
+//   }
+
+  static moveDodgerLeft() {
+    let spaceship = document.getElementById("spaceship")
+    let leftNumbers = spaceship.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        spaceship.style.left = `${left - 4}px`;
+    }
+  }
+
+  static moveDodgerRight() {
+    let spaceship = document.getElementById("spaceship")
+    let leftNumbers = spaceship.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10)
+
+    if (left < 360) {
+      spaceship.style.left = `${left + 4}px`
+    }
+  }
+
+  static moveDodgerUp() {
+    let spaceship = document.getElementById("spaceship")
+    let leftNumbers = spaceship.style.bottom.replace("px", "")
+    let left = parseInt(leftNumbers, 10)
+  
+    if (left < 375) {
+      spaceship.style.bottom = `${left + 4}px`
+    }
+  }
+
+  static moveDodgerDown() {
+    let spaceship = document.getElementById("spaceship")
+    let leftNumbers = spaceship.style.bottom.replace("px", "")
+    let left = parseInt(leftNumbers, 10)
+  
+    if (left > 0) {
+      spaceship.style.bottom = `${left - 4}px`
+    }
+  }
+
+}
+
+
+
+
+
