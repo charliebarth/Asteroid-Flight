147d5bcfe4b378a0a9e2a1b556a25fbadcbbedec
github username <charliepbarth@gmail.com>
2021-02-02 22:42:35 -0500

    Added ateroid move function

---
+++ frontend/scripts/models/asteroid.js
@@ -0,0 +1,26 @@
+class Asteroid {
+    // static moveAsteroid() {
+    //     // let rock = document.getElementById(`asteroid${Helpers.applygetRndInteger(1, 6)}`)
+    //     let rock = document.getElementById("asteroid1")
+    //     let nums = rock.style.top.replace("px", "")
+    //     let place = parseInt(nums, 10)
+    
+    //     if (place > ) {
+    //       rock.style.top = `${place + 4}px`
+    //     }
+    //     
+    // }
+    static moveAsteroid() {
+        setTimeout(function(){
+            let rock = document.getElementById("asteroid1")
+            let topNumbers = rock.style.top.replace("px", "")
+            let top = parseInt(topNumbers, 10)
+    
+            if (top < 510) {
+                rock.style.top = `${top + 4}px`
+            }
+    
+            return top
+        }, 500)
+    }
+}
