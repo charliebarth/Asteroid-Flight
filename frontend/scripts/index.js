85b00536f91ae5bb2fa985b8f5c80a2b5a66bcc3
github username <charliepbarth@gmail.com>
2021-02-02 20:20:30 -0500

    Moved methods from index.js to spaceship.js

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,56 +1,14 @@
-let dodger = document.getElementById("dodger");
-
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
-
-  if (left > 0) {
-    dodger.style.left = `${left - 4}px`;
-    
-  }
-}
-
-function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace("px", "")
-  let left = parseInt(leftNumbers, 10)
-
-  if (left < 360) {
-    dodger.style.left = `${left + 4}px`
-  }
-
-}
-
-function moveDodgerUp() {
-    let leftNumbers = dodger.style.bottom.replace("px", "")
-    let left = parseInt(leftNumbers, 10)
-  
-    if (left < 375) {
-      dodger.style.bottom = `${left + 4}px`
-    }
-  
-}
-
-function moveDodgerDown() {
-    let leftNumbers = dodger.style.bottom.replace("px", "")
-    let left = parseInt(leftNumbers, 10)
-  
-    if (left > 0) {
-      dodger.style.bottom = `${left - 4}px`
-    }
-  
-}
-
 document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-  if (e.key === "ArrowRight"){
-    moveDodgerRight();
-  }
-  if (e.key === "ArrowUp"){
-    moveDodgerUp();
-  }
-  if (e.key === "ArrowDown"){
-    moveDodgerDown();
-  }
+    if (e.key === "ArrowLeft") {
+        Spaceship.moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight"){
+        Spaceship.moveDodgerRight();
+    }
+    if (e.key === "ArrowUp"){
+        Spaceship.moveDodgerUp();
+    }
+    if (e.key === "ArrowDown"){
+        Spaceship.moveDodgerDown();
+    }
 });
