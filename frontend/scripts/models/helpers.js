29d0b4edf4a5e078e3327a4c21b8c61cff1e99c3
github username <charliepbarth@gmail.com>
2021-02-04 14:18:39 -0500

    Added game over image and find collision function

--- frontend/scripts/models/helpers.js
+++ frontend/scripts/models/helpers.js
@@ -2,4 +2,12 @@ class Helpers {
     static getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min) ) + min;
     }
+
+    static findCollision(rect1, rect2) {
+        let box = document.getElementById("GameOver")
+        if (rect1._x < rect2.style.left + rect2.style.width && rect1._x + rect1._width > rect2.style.left && rect1._y < rect2.style.top + rect2.style.height && rect1._height + rect1._y > rect2.style.top) {
+            box.style.top = "200px"
+            box.style.left = "400px"
+        }
+    }
 }
