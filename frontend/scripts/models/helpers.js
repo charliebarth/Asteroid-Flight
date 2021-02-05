72dfa03ae7ac964eaba0d605f9baa310da7f1e17
github username <charliepbarth@gmail.com>
2021-02-04 22:21:19 -0500

    Added a collision helper function

--- frontend/scripts/models/helpers.js
+++ frontend/scripts/models/helpers.js
@@ -4,8 +4,9 @@ class Helpers {
     }
 
     static findCollision(rect1, rect2) {
-        let box = document.getElementById("GameOver")
+        const box = document.getElementById("GameOver")
         if (rect1._x < rect2.style.left + rect2.style.width && rect1._x + rect1._width > rect2.style.left && rect1._y < rect2.style.top + rect2.style.height && rect1._height + rect1._y > rect2.style.top) {
+            rect1._collided = true
             box.style.top = "200px"
             box.style.left = "400px"
         }
