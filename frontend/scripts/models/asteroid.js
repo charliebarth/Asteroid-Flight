6ab93b8e84749559942278346fc8cab7bc33b1dc
github username <charliepbarth@gmail.com>
2021-02-05 00:09:36 -0500

    Rebuilt moveAsteroid

--- frontend/scripts/models/asteroid.js
+++ frontend/scripts/models/asteroid.js
@@ -2,27 +2,44 @@ class Asteroid {
     constructor(star){
         const starStyle = getComputedStyle(star)
         this.name = star.id
-        this._width = starStyle.width
-        this._hieght = starStyle.height
-        this._left = starStyle.left
-        this._bottom = starStyle.bottom
         this._collided = false
+
+        this._width = parseInt(starStyle.width.replace("px", ""), 10)
+        this._height = parseInt(starStyle.height.replace("px", ""), 10)
+        this._left = parseInt(starStyle.left.replace("px", ""), 10)
+        this._bottom = parseInt(starStyle.bottom.replace("px", ""), 10)
+        
+        this._right = (this._left - this._width);
+        this._top = (this._height + this._bottom);
+        
     }
 
     moveAsteroid() {
         let movement = Helpers.getRndInteger(2, 6);
-        const objPlace = document.getElementById(`${this.name}`)
+
+        const objPlace = document.getElementById(`${this.name}`);
+        const starStyle = getComputedStyle(objPlace);
         const rock = this;
+
         let timer = setInterval (function () {
-            let topNumbers = rock._bottom.replace("px", "")
-            let xPlace = parseInt(topNumbers, 10)
+
             if (rock._collided === true) {
                 clearInterval(timer);
-            } else if (xPlace > 20) {
-                rock._bottom = `${xPlace - movement}px`
-                objPlace.style.bottom = `${xPlace - movement}px`
-            } else if (xPlace <= 20) {
-                rock._bottom = "470px"
+            
+            } else if (rock._bottom > 20) {
+                rock._width = parseInt(starStyle.width.replace("px", ""), 10)
+                rock._height = parseInt(starStyle.height.replace("px", ""), 10)
+                rock._left = parseInt(starStyle.left.replace("px", ""), 10)
+                rock._bottom = parseInt(starStyle.bottom.replace("px", ""), 10)
+                
+                rock._right = (rock._left - rock._width);
+                rock._top = (rock._height + rock._bottom);
+
+                objPlace.style.bottom = `${rock._bottom - movement}px`
+                //Helpers.findCollision(rock)
+            
+            } else if (rock._bottom <= 20) {
+                rock._bottom = 470
                 objPlace.style.bottom = "470px"
             }
         }, 100);
