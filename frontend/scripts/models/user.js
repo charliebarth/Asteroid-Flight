eec92699c4a031f2e9997086656b8f926359e178
github username <charliepbarth@gmail.com>
2021-02-06 23:47:05 -0500

    Added user login function

--- frontend/scripts/models/user.js
+++ frontend/scripts/models/user.js
@@ -1,10 +1,31 @@
 class User {
-    constructor (name) {
-        this._name = name;
+    // constructor (name) {
+    //     this._name = name;
+    // }
+
+    // set userId(id) {
+    //     this._id = id
+    // }
+
+    postUser = username => {
+        API.postUser(username)
+        .then(userData => {
+          this.updateInfo(userData.data)
+        })
+      }
+    
+    updateInfo = data => {
+        this.id = data.id
+        this.username = data.attributes.name
     }
 
-    set userId(id) {
-        this._id = id
+    static login (event) {
+        event.preventDefault();
+        const user = new User;
+        const username = document.getElementById("username-input").value;
+        //if (user.postUser(username)) {userForm.style.display = "none"};
+        user.postUser(username);
+        userForm.style.display = "none"
     }
 
 }
