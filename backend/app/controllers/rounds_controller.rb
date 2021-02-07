87f792f1b3d5bee911fc5a834d06b0ba5dc7141d
github username <charliepbarth@gmail.com>
2021-02-06 20:35:59 -0500

    Created round controller

--- backend/app/controllers/rounds_controller.rb
+++ backend/app/controllers/rounds_controller.rb
@@ -1,2 +1,17 @@
 class RoundsController < ApplicationController
+    def create
+        round = Round.create(round_params)
+        render json: RoundSerializer.new(round).serializable_hash
+      end
+  
+      def index
+        rounds = Round.all
+        render json: RoundSerializer.new(rounds).serializable_hash
+      end
+    
+      private
+    
+      def round_params
+        params.require(:round).permit(:score, :user_id)
+      end
 end
