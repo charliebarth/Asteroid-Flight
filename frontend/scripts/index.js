e3f5452f5bee85e7784bb098cc6e65cde7834ee4
github username <charliepbarth@gmail.com>
2021-02-07 13:42:44 -0500

    Added variables to all divs in html body

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -2,12 +2,15 @@ const api = API.startWithPort()
 const newGame = new Game
 let currentGame = ""
 let num = 0
-const game = document.getElementById("game")
+const gameDiv = document.getElementById("game")
+const loginDiv = document.getElementById("login")
+const startDiv = document.getElementById("start")
 const asteroidArray = []
 const userForm = document.getElementById("username-form")
 
 document.addEventListener("DOMContentLoaded", function(){
-    game.style.display = 'none';
+    gameDiv.style.display = 'none';
+    startDiv.style.display = 'none';
 
 });
 
@@ -17,32 +20,32 @@ userForm.addEventListener("submit", User.login);
 //     delete keysPressed[e.key];
 // });
 
-document.addEventListener('keydown', (e) => {
-    // keysPressed[e.key] = true;
-    // if (keysPressed['ArrowLeft'] && e.key == 'ArrowUp' || keysPressed['ArrowUp'] && e.key == 'ArrowLeft') {
-    //     Spaceship.moveDodgerLeft();
-    //     Spaceship.moveDodgerUp();
-    // } else if (keysPressed['ArrowLeft'] && e.key == 'ArrowDown'|| keysPressed['ArrowDown'] && e.key == 'ArrowLeft') {
-    //     Spaceship.moveDodgerLeft();
-    //     Spaceship.moveDodgerDown();
-    // } else if (keysPressed['ArrowRight'] && e.key == 'ArrowUp'|| keysPressed['ArrowUp'] && e.key == 'ArrowRight') {
-    //     Spaceship.moveDodgerRight();
-    //     Spaceship.moveDodgerUp();
-    // } else if (keysPressed['ArrowRight'] && e.key == 'ArrowDown'|| keysPressed['ArrowDown'] && e.key == 'ArrowRight') {
-    //     Spaceship.moveDodgerRight();
-    //     Spaceship.moveDodgerDown();
-    // } else if (e.key === "ArrowLeft") {
-    //     Spaceship.moveDodgerLeft();
-    // } else if (e.key === "ArrowRight"){
-    //     Spaceship.moveDodgerRight();
-    // } else if (e.key === "ArrowUp"){
-    //     Spaceship.moveDodgerUp();
-    // } else if (e.key === "ArrowDown"){
-    //     Spaceship.moveDodgerDown();
-    if (e.key === "W"){
-        game.style.display = 'block';
-        Game.launchGame();
-    }
-});
+// document.addEventListener('keydown', (e) => {
+//     keysPressed[e.key] = true;
+//     if (keysPressed['ArrowLeft'] && e.key == 'ArrowUp' || keysPressed['ArrowUp'] && e.key == 'ArrowLeft') {
+//         Spaceship.moveDodgerLeft();
+//         Spaceship.moveDodgerUp();
+//     } else if (keysPressed['ArrowLeft'] && e.key == 'ArrowDown'|| keysPressed['ArrowDown'] && e.key == 'ArrowLeft') {
+//         Spaceship.moveDodgerLeft();
+//         Spaceship.moveDodgerDown();
+//     } else if (keysPressed['ArrowRight'] && e.key == 'ArrowUp'|| keysPressed['ArrowUp'] && e.key == 'ArrowRight') {
+//         Spaceship.moveDodgerRight();
+//         Spaceship.moveDodgerUp();
+//     } else if (keysPressed['ArrowRight'] && e.key == 'ArrowDown'|| keysPressed['ArrowDown'] && e.key == 'ArrowRight') {
+//         Spaceship.moveDodgerRight();
+//         Spaceship.moveDodgerDown();
+//     } else if (e.key === "ArrowLeft") {
+//         Spaceship.moveDodgerLeft();
+//     } else if (e.key === "ArrowRight"){
+//         Spaceship.moveDodgerRight();
+//     } else if (e.key === "ArrowUp"){
+//         Spaceship.moveDodgerUp();
+//     } else if (e.key === "ArrowDown"){
+//         Spaceship.moveDodgerDown();
+//     if (e.key === "W"){
+//         game.style.display = 'block';
+//         Game.launchGame();
+//     }
+// });
 
 
